a41a2a6bbd91e68fb7d236a6cfa0d801ea1a2691
ekko-wang <15611637909@163.com>
2024-03-05 20:13:16 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 冷酷的小胖 on 2024/3/4.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
